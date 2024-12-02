912ca30ba337616bf2d7e9639a4cf97778d28712
Allen <allengnr@gmail.com>
2024-12-02 20:06:53 +0800

    Update embed_resource to 3.0.1

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 extern crate embed_resource;
 fn main() {
     println!("cargo:rerun-if-changed=gui/mainview.fl");
-    embed_resource::compile("PngOptimizer.rc", embed_resource::NONE);
+    embed_resource::compile("PngOptimizer.rc", embed_resource::NONE)
+        .manifest_required()
+        .unwrap();
 }
